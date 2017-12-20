244c76a2b773c545eee823f17a8da21ccfc43e58
supfire <supfire@sohu.com>
2017-12-20 18:12:16 +0800

    整个程序1.0版本由vs2008升级为vs2010工具可用

---
+++ test-lib-.net-v2.0-vs2010/test/src/Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AndyLib.Util;
+namespace Test
+{
+    class Test
+    {
+        public static String httpPost()
+        {
+            //提交网址设置
+            String URL = "https://www.baidu.com";
+            String userName = "libing";
+            String postStr = "&userName=" + userName;
+            //推送
+            String returnMsg = HttpPost.PostData(URL, postStr);
+            //返回结果
+            return returnMsg;
+        }
+
+        public static String MD5()
+        {
+            String password = "123456";
+            //MD5加密
+            return md5.getMd5Hash(password);
+        }
+
+        public static String RSASign()
+        {
+            //签名参数
+            String userName = "李兵";
+            String Str = "123456";
+            //构造java数据签名
+            String sign = utilRSASign.RSASign(Properties.Settings.Default.privateKey, userName + Str);
+            //返回结果
+            return sign;
+        }
+
+        public static String RSAEncrypt()
+        {
+            //参数
+            String userName = "李兵";
+            String Str = "123456";
+            //构造java数据加密
+            String encrypt = utilRSASign.RSAEncrypt(Properties.Settings.Default.publicKey, userName + Str);
+            //返回结果
+            return encrypt;
+        }
+
+        public static String SM3()
+        {
+            //参数
+            String Str = "123456";
+            //构造数据加密
+            String encrypt = SM3Digest.SM3(Str);
+            //返回结果
+            return encrypt;
+        }
+    }
+}
