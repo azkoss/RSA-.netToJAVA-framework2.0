34696a1b5c0e6e00132105630de073d130840f15
supfire <supfire@sohu.com>
2017-12-21 18:10:47 +0800

    sm3加密utf8矫正

--- test-lib-.net-v2.0/test/src/Test.cs
+++ test-lib-.net-v2.0/test/src/Test.cs
@@ -5,7 +5,18 @@ using AndyLib.Util;
 namespace Test
 {
     class Test
-    {
+    {    
+        /// <summary>
+        /// 测试类集合
+        /// </summary>      
+        /// <remarks>
+        /// 2017.12.01: 创建. 李兵 <br/>
+        /// </remarks>
+
+        /// <summary>
+        /// httpPost提交
+        /// </summary>
+        /// <returns></returns>
         public static String httpPost()
         {
             //提交网址设置
@@ -17,42 +28,43 @@ namespace Test
             //返回结果
             return returnMsg;
         }
-
+        //MD5加密
         public static String MD5()
         {
+            String userName = "李兵";
             String password = "123456";
             //MD5加密
-            return md5.getMd5Hash(password);
+            return md5.getMd5Hash(userName + password);
         }
-
+        //RSA签名
         public static String RSASign()
         {
             //签名参数
             String userName = "李兵";
-            String Str = "123456";
+            String password = "123456";
             //构造java数据签名
-            String sign = utilRSASign.RSASign(Properties.Settings.Default.privateKey, userName + Str);
+            String sign = utilRSASign.RSASign(Properties.Settings.Default.privateKey, userName + password);
             //返回结果
             return sign;
         }
-
+        //RSA加密
         public static String RSAEncrypt()
         {
             //参数
             String userName = "李兵";
-            String Str = "123456";
+            String password = "123456";
             //构造java数据加密
-            String encrypt = utilRSASign.RSAEncrypt(Properties.Settings.Default.publicKey, userName + Str);
+            String encrypt = utilRSASign.RSAEncrypt(Properties.Settings.Default.publicKey, userName + password);
             //返回结果
             return encrypt;
         }
-
+        //SM3加密
         public static String SM3()
         {
             //参数
-            String Str = "123456";
+            String str = "李兵123456";
             //构造数据加密
-            String encrypt = SM3Digest.SM3(Str);
+            String encrypt = SM3Digest.SM3(str);
             //返回结果
             return encrypt;
         }
